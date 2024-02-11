e113583544e613fefbf0307f6664a6e914d3090a
Dmitry Marov <d.marov94@gmail.com>
2024-02-11 14:42:24 +0300

    improved windows installer

--- install-windows.ps1
+++ install-windows.ps1
@@ -11,15 +11,16 @@ New-Item -Path "~/AppData/Local/nvim" -ItemType SymbolicLink -Value "~/repos/neo
 # :source .\lua\plugins-install.lua
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
 
-choco install python3 neovim lua-language-server nvm
+choco install python312 neovim lua-language-server nvm
 # need add write permission for C:\ProgramData\chocolatey\lib\lua-language-server\tools\x64\log manually
 
 nvm install 18.19.0
 nvm use 18.19.0
+npm install -g neovim
 
 pip install neovim
 
-nvim -c :PackerSync -c :q
+nvim -c ":source .\lua\plugins-install.lua" -c :PackerSync -c :q
 
 npm i -g typescript-language-server `
  vscode-langservers-extracted `
