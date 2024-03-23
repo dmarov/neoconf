e58beeb9ae2c23324366a2e6afe4ebfcbe562e87
Dmitry Marov <d.marov94@gmail.com>
2024-03-23 14:18:59 +0300

    install script wip

--- scripts/install.js
+++ scripts/install.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+import { readdirSync, statSync } from 'fs';
+
+const fromDir = './config-dir/';
+
+const toDir = 'C:/Users/mds/AppData/local/nvim/';
+
+const getFilesRecursive = (dir, filelist) => {
+  const files = readdirSync(dir);
+
+  files.forEach((file) => {
+    if (statSync(`${dir}${file}`).isDirectory()) {
+      filelist = getFilesRecursive(`${dir}${file}/`, filelist);
+    } else {
+      filelist.push(`${dir}${file}`);
+    }
+  });
+
+  return filelist;
+};
+
+const res = getFilesRecursive(fromDir, []);
+
+console.log(res);
